1406f295001c6adbde2e5f65882691f6ec1bddb5
Anh-Kiet Tong <ptong@unc.edu>
2024-12-01 17:59:07 -0500

    implement feed pg & post-creator & add routes

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component,inject} from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
-
+import { MatDialog } from '@angular/material/dialog';
+import { PostCreatorComponent } from './shared/post-creator/post-creator.component';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
@@ -12,4 +13,13 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css',
   imports: [MatButtonModule, MatIconModule, RouterLink, RouterOutlet],
 })
-export class AppComponent {}
+export class AppComponent {
+  readonly dialog = inject(MatDialog);
+
+  openPostCreator() {
+    const dialogRef = this.dialog.open(PostCreatorComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }}
